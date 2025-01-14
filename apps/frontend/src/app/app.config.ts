fb3b6bd3ca930d71d0be4aba6ba798710d8cbb8d
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-14 18:48:51 -0500

    Configuración de la app fue cambiada

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,10 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth } from '@angular/fire/auth';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBEwHp2jBidsdjhFsRc_IDQgOm47g5Hx0A',
+  authDomain: 'c23-100-webapp-a4a86.firebaseapp.com',
+  projectId: 'c23-100-webapp-a4a86',
+  storageBucket: 'c23-100-webapp-a4a86.firebasestorage.app',
+  messagingSenderId: '195033874086',
+  appId: '1:195033874086:web:7244cc41155fefef21be67',
+  measurementId: 'G-XRPGE3V6B9',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideHttpClient(withFetch()),
   ],
 };
--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,29 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
-import { LoginComponent } from './app/auth/login.component';
-import { RegisterComponent } from './app/auth/register.component';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBEwHp2jBidsdjhFsRc_IDQgOm47g5Hx0A",
-  authDomain: "c23-100-webapp-a4a86.firebaseapp.com",
-  projectId: "c23-100-webapp-a4a86",
-  storageBucket: "c23-100-webapp-a4a86.firebasestorage.app",
-  messagingSenderId: "195033874086",
-  appId: "1:195033874086:web:7244cc41155fefef21be67",
-  measurementId: "G-XRPGE3V6B9"
-};
-
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes),provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth(),), provideHttpClient(), 
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
