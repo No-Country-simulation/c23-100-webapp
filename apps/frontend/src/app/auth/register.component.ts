e136019254fa042edbbfc928b71a2a271b0ab147
Seba Olima <edificarvcp@gmail.com>
2025-01-14 21:35:38 -0300

    agregando campo de confirmacion de contraseña en el registro de usuario y sus respectivas validaciones

--- apps/frontend/src/app/auth/register.component.ts
+++ apps/frontend/src/app/auth/register.component.ts
@@ -3,35 +3,41 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 
-
-
 @Component({
   selector: 'app-register',
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css',
-  
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
-    this.registerForm = this.fb.group({
-      /*username: ['', [Validators.required, Validators.minLength(3)]],*/
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
+    this.registerForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(group: FormGroup): { [key: string]: boolean } | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
   }
 
   onSubmit(): void {
     if (this.registerForm.valid) {
-      const {  email, password } = this.registerForm.value;
+      const { email, password } = this.registerForm.value;
       this.authService.register(email, password).then(
         (userCredential) => {
           const user = userCredential.user;
           const createUserDto = { email, password };
-  
+
           this.authService.createUser(createUserDto).then(
             (response) => {
               console.log('Usuario creado en el backend:', response);
@@ -47,10 +53,4 @@ export class RegisterComponent {
       );
     }
   }
-  
-    }
-  
-  
-
-
-
+}
