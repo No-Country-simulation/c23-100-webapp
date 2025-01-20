5732a0a312f6db0288450fac655bd349dee7c3b0
Martin Quiroz <martinquiroz_18@hotmail.com>
2025-01-19 23:53:16 -0300

    Modificaciones de panel de usuario

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
+import { UserPanelComponent } from './user-panel/user-panel.component';
 
 export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'user-panel', component: UserPanelComponent },
   { path: '', component: HomeComponent },
 ];
