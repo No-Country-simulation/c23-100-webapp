ffced2e83e7e704b39c7e842a7f6660777a0a17d
Martin Quiroz <martinquiroz_18@hotmail.com>
2025-01-19 22:48:34 -0300

    Panel de usuario

---
+++ apps/frontend/src/app/user-panel/user-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-panel',
+  imports: [CommonModule],
+  templateUrl: './user-panel.component.html',
+  styleUrl: './user-panel.component.css',
+})
+export class UserPanelComponent {}
