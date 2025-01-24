d18159ddb96045488dec213c0e1f4dbb4660b71e
Martin Quiroz <martinquiroz_18@hotmail.com>
2025-01-24 12:08:25 -0300

    Cambios en user panel

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { UserPanelComponent } from './user-panel/user-panel.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
