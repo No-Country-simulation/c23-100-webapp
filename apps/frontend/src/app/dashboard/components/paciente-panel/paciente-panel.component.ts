e27e6e4bc62edfed61aad75a7db897ed34a00049
Martin Quiroz <martinquiroz_18@hotmail.com>
2025-02-01 19:56:49 -0300

    Lista de citas pendientes

--- apps/frontend/src/app/dashboard/components/paciente-panel/paciente-panel.component.ts
+++ apps/frontend/src/app/dashboard/components/paciente-panel/paciente-panel.component.ts
@@ -2,18 +2,36 @@ import { Component, HostListener, input, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { User } from '../../../shared';
+import { Doctor } from '../../../shared/interfaces/doctor';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-paciente-panel',
-  imports: [ ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './paciente-panel.component.html',
   styleUrl: './paciente-panel.component.css',
 })
 export class PacientePanelComponent implements OnInit {
-  public user = input.required<User>();
 
+  public user = input.required<User>();
   isSidebarOpen = true; // Cambiado a false por defecto
   isSidebarHalfOpen = false;
+  registrosCitas:any[];
+  usuario:User;
+  doctor1:Doctor;
+  doctor2:Doctor;
+  doctor3:Doctor;
+
+  constructor() {
+    this.doctor1 = {name: "Jose Roberto", photo: "jose-roberto.jpg"};
+    this.doctor2 = {name: "Mila Mesa", photo: "mila-mesa.jpg"};
+    this.doctor3 = {name: "Roberto Archundia", photo: "roberto-archundia.jpg"};
+    this.registrosCitas = [
+      {usuario: this.usuario, doctor: this.doctor1, horario: "18:30 pm", direccion: "Av. Rivadavia 1000", pasillo: "C", numeroPuerta: 14, msj: "12 horas de ayuna antes y despues de asistir al turno."},
+      {usuario: this.usuario, doctor: this.doctor2, horario: "4:30 pm", direccion: "Av. Tucasa 123", pasillo: "A", numeroPuerta: 2, msj: "Tome mucha agua y sientese si le cuesta respirar."},
+      {usuario: this.usuario, doctor: this.doctor3, horario: "1:00 pm", direccion: "Rodney 5222", pasillo: "B", numeroPuerta: 17, msj: "Venga con 2 horas de anticipación para firmar unos papeles."}
+    ];
+  }
 
   ngOnInit() {
     this.checkScreenWidth();
---
+++ apps/frontend/src/app/shared/interfaces/doctor.ts
@@ -0,0 +1,5 @@
+export interface Doctor {
+    name: string;
+    photo: string;
+}
+  
