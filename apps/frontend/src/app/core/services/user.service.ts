d5eafc1772c2f3cbc01cd1ff93bb7ae793fa9b4e
JesusMedina21 <jesusmedina0921@gmail.com>
2025-02-03 22:02:34 -0400

    cambio de sessionStorage a localStorage para poder hacer nuevas peticiones en nuevas pestañas

--- apps/frontend/src/app/core/services/user.service.ts
+++ apps/frontend/src/app/core/services/user.service.ts
@@ -32,7 +32,7 @@ export class UserService {
   createAppointment(appointmentData: any): Observable<any> {
     return this.http.post(this.appointmentUrl, appointmentData, {
       headers: {
-        Authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
+        Authorization: `Bearer ${localStorage.getItem('userToken')}`,
       },
     });
   }
