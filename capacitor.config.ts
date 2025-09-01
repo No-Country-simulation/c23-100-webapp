1a59ddc3383793682d261bd04c58a706bdf91414
JesusMedina21 <jesusmedina0921@gmail.com>
2025-09-01 18:22:40 -0400

    generacion de plataforma android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Medical APP',
+  webDir: 'dist/apps/frontend/browser'
+};
+
+export default config;
+//
